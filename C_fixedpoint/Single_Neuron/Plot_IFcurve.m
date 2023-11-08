b975712c90103e26777edd2ce390037c8450eb15
Praveen raj Ayyappan <88020935+PraveenRajAyyappan@users.noreply.github.com>
2023-11-08 01:15:38 -0500

    Added fxp IFcurve_plot

---
+++ C_fixedpoint/Single_Neuron/Plot_IFcurve.m
@@ -0,0 +1,17 @@
+clear, clc;
+
+idx = 45:10:161;
+
+for i = 1:length(idx)
+    data = importdata("Vmem_Iin_"+num2str(idx(i))+".txt");
+    [peak_amplitudes, peak_indices] = findpeaks(data(:,1),'MinPeakProminence',1);
+    num_peaks(i)= length(peak_amplitudes);
+    firingrate(i) = 1/(median(diff(peak_indices))*0.001e-3);
+        
+end
+
+%Plot the IF curve
+figure, plot(idx,firingrate,'k-o')
+xlabel('Input Current (pA)');
+ylabel('Spike Frequency (Hz)');
+
